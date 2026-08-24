5ccf1966ae08028d19bf706952769f912c3e08c5
Bryan Calderon <bryancalderon1001@gmail.com>
2026-08-24 09:52:36 -0500

    Add VNet Flow Logs with Traffic Analytics to Log Analytics workspace

--- terraform/monitoring.tf
+++ terraform/monitoring.tf
@@ -30,3 +30,44 @@ resource "azurerm_monitor_diagnostic_setting" "activity_log" {
     category = "Security"
   }
 }
+
+data "azurerm_network_watcher" "this" {
+  name                = "NetworkWatcher_southcentralus"
+  resource_group_name = "NetworkWatcherRG"
+}
+
+resource "random_string" "storage_suffix" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
+resource "azurerm_storage_account" "flowlogs" {
+  name                     = "stflowlogs${random_string.storage_suffix.result}"
+  resource_group_name      = azurerm_resource_group.this.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_network_watcher_flow_log" "this" {
+  name                 = "Flow_Logs_NWatcher"
+  network_watcher_name = data.azurerm_network_watcher.this.name
+  resource_group_name  = data.azurerm_network_watcher.this.resource_group_name
+  target_resource_id   = azurerm_virtual_network.vnet.id
+  storage_account_id   = azurerm_storage_account.flowlogs.id
+  enabled              = true
+
+  retention_policy {
+    enabled = true
+    days    = 30
+  }
+
+  traffic_analytics {
+    enabled                = true
+    workspace_id           = azurerm_log_analytics_workspace.this.workspace_id
+    workspace_region       = azurerm_log_analytics_workspace.this.location
+    workspace_resource_id  = azurerm_log_analytics_workspace.this.id
+    interval_in_minutes    = 60
+  }
+}
